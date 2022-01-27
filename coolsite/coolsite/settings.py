d0738476f17eec647c61d702799d14cf16d993e3
RafilGalimzyanov <54663137+RafilGalimzyanov@users.noreply.github.com>
2022-01-27 16:50:09 +0300

    1

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -122,7 +122,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
 
 
-# Default primary key field type
+
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
