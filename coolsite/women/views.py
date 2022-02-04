5f058c194ccd4d2b0404a159f3f77e9065c1cb72
RafilGalimzyanov <54663137+RafilGalimzyanov@users.noreply.github.com>
2022-02-04 23:14:38 +0300

    1

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -17,7 +17,7 @@ def about(request):
     return render(request, 'women/about.html', {'menu': menu ,'title': 'О сайтее'})
 
 def archive(request, year):
-    if int(year) > 2060:
+    if int(year) > 200:
         return redirect('home', permanent=True)
     return HttpResponse(f'Архив - {year}.')
 
