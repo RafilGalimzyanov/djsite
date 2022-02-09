c706b88a4b487bbe7c5987c17c03c28ff98364e9
RafilGalimzyanov <54663137+RafilGalimzyanov@users.noreply.github.com>
2022-02-09 22:52:01 +0300

    1

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', index, name = 'home'),
     path('cats/<int:catid>/', categories),
     path('about/', about, name='about'),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive)
+    re_path(r'^archive/(?P<year>[0-8]{4})/', archive)
 ]
